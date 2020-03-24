a0403ae2c070245cc5d3de5c841c8a2775f3f8d7
Jose Andres Moncada Quintero <1130613425@icesi.edu.co>
2020-03-24 12:29:25 -0500

    update automplete at login

--- scripts/select_interaction.js
+++ scripts/select_interaction.js
@@ -44,7 +44,7 @@ $(document).ready(function () {
     });
 
     $('.select2__selector--city').on('select2:close', function (e) {
-        let value = $('.select2__selector--sector').select2('data')[0].id;
+        let value = $('.select2__selector--city').select2('data')[0].id;
         if(value === ''){
             document.querySelector('.select__label--city').classList.add('select__label--register');
         document.querySelector('.select__label--city').classList.remove(
