5ff4fb7f3768da86724d9601dd1e63d8bb3ad2c3
juanbravozu <juanda.bravo98@gmail.com>
2020-04-04 12:56:57 -0500

    Request Details interaction

--- scripts/dashboard_reqDetails.js
+++ scripts/dashboard_reqDetails.js
@@ -10,6 +10,25 @@ function handleLoad() {
         if(event.target.closest('.status') != status)
         statusList.classList.remove('status__list--active');
     });
+
+    const candidateBtn = document.querySelector('.reqDetails__candidate');
+    const requestBtn = document.querySelector('.reqDetails__request');
+    const candidatesContainer = document.querySelector('.reqDetails__candidates');
+    const requestContainer = document.querySelector('.reqDetails__details');
+
+    candidateBtn.addEventListener('click', () => {
+        candidateBtn.classList.add('reqDetails__contentCategory--active');
+        requestBtn.classList.remove('reqDetails__contentCategory--active');
+        candidatesContainer.classList.add('reqDetails__content--active');
+        requestContainer.classList.remove('reqDetails__content--active');
+    });
+
+    requestBtn.addEventListener('click', () => {
+        candidateBtn.classList.remove('reqDetails__contentCategory--active');
+        requestBtn.classList.add('reqDetails__contentCategory--active');
+        candidatesContainer.classList.remove('reqDetails__content--active');
+        requestContainer.classList.add('reqDetails__content--active');
+    });
 }
 
 window.addEventListener('load', handleLoad);
