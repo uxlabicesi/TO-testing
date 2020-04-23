2b49ca3ba39f3978490349ed796254888bcdfee7
Jose Andres Moncada Quintero <1130613425@icesi.edu.co>
2020-04-23 18:30:26 -0500

    updating values for popup

---
+++ scripts/samples/sample_setCandidateEvaluation.js
@@ -0,0 +1,28 @@
+window.addEventListener('load', () => {
+
+
+    window.onclick = function(event) {}
+
+    const candidateEvaluationBtn = document.querySelector('.btn__evaluation');
+    // use these for get the values... 
+    const rangeInputExperience = document.getElementById('range_experience');
+    const rangeInputSkills = document.getElementById('range_skills');
+    const rangeInputEducation = document.getElementById('range_education');
+
+    let informationPopUp = {
+        title: "Evaluación completa",
+        text: "Mejoramos cada día para brindarte el mejor talento",
+        btn: "Continuar",
+        goTo: "",
+    };
+
+    let rootForPopUp = ".popUp";
+
+    candidateEvaluationBtn.addEventListener('click', (ev) => {
+        console.log("----------------------------------------");
+        createPopUp(informationPopUp, rootForPopUp);
+        candidateEvaluationBtn.innerHTML="Reevaluar candidato";
+        ev.preventDefault();
+    });
+
+});
