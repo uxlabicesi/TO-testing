15c63e5645e572e74f1d4963a309953f84ed7564
Jose Andres Moncada Quintero <1130613425@icesi.edu.co>
2020-04-24 22:48:35 -0500

    update sample for detailed information candidate

--- scripts/samples/sample_candidateDetailsInformation.js
+++ scripts/samples/sample_candidateDetailsInformation.js
@@ -20,9 +20,9 @@ var sample = {
     age: 32,
     city: 'Cali, Valle del Cauca',
     exp: [
-        {position:'Experiencia Laboral 1',timeInMonths:24,enterprise:'empresa 1',duties:"Lorem ipsum loen dae, Lorem ipsum loen dae, Lorem ipsum loen dae." }, 
-        {position:'Experiencia Laboral 2',timeInMonths:12,enterprise:'empresa 2',duties:"Lorem ipsum loen dae, Lorem ipsum loen dae, Lorem ipsum loen dae." },
-        {position:'Experiencia Laboral 3',timeInMonths:6,enterprise:'empresa 3',duties:"Lorem ipsum loen dae, Lorem ipsum loen dae, Lorem ipsum loen dae." },
+        {position:'Experiencia Laboral 1',timeInMonths:24,enterprise:'empresa 1',duties:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." }, 
+        {position:'Experiencia Laboral 2',timeInMonths:12,enterprise:'empresa 2',duties:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." },
+        {position:'Experiencia Laboral 3',timeInMonths:6,enterprise:'empresa 3',duties:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." },
         ],
         /* exp: [
         'Experiencia Laboral 1', 
