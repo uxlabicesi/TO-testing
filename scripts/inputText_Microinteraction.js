12faafb251d1dc4a6ef1439fda65a07ff784baf2
Jose Andres Moncada Quintero <1130613425@icesi.edu.co>
2020-04-29 15:59:44 -0500

    fix the autocomplete problem, starts manually the feedback event when the inputs is not empty

--- scripts/inputText_Microinteraction.js
+++ scripts/inputText_Microinteraction.js
@@ -10,6 +10,24 @@ window.addEventListener('load', () => {
 
     inputs.forEach(input => {
 
+        
+        // the following fragment fix the autocomplete problem, starts manually the feedback event when the inputs is not empty
+        if(input.value !== ""){
+            input.parentElement.classList.add('textInput--focused');
+            let label = input.parentElement.querySelector('label');
+            if(label!=null){
+                label.classList.remove('label--none');
+                label.classList.add('label--active');
+            }
+        }else{
+            let label = input.parentElement.querySelector('label');
+            if(!input.value && label!=null) {
+                label.classList.remove('label--active');
+                input.parentElement.classList.remove('textInput--focused');
+            }
+        }
+
+
         // Activates the microinteraction when the input is focused
         input.addEventListener('focus', (event) => {
             event.target.parentElement.classList.add('textInput--focused');
