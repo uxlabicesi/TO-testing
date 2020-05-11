96b63f6c91ec5b2660772e01ddced06f2efedbe0
josemoncada87 <josemoncada87@hotmail.com>
2020-05-11 18:16:43 -0500

    fix console.log message

--- scripts/samples/sample_autoCompleteAndClean.js
+++ scripts/samples/sample_autoCompleteAndClean.js
@@ -3,7 +3,6 @@ window.addEventListener('load', () => {
 
     autocompleteButton.addEventListener('click', (ev) => {
        ev.preventDefault();
-       //console.log("go to autocomplete");
     });
 
     const cleanFormButton = document.querySelector('.reqCreation__clear');
