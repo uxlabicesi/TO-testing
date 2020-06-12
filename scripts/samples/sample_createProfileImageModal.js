b1ad15042372163a1c4a37a83d094ad3ecd8851a
juanbravozu <juanda.bravo98@gmail.com>
2020-06-12 16:59:44 -0500

    Changed crop profle img function from modal to part of the main view

--- scripts/samples/sample_createProfileImageModal.js
+++ scripts/samples/sample_createProfileImageModal.js
@@ -4,16 +4,10 @@ window.addEventListener('load', () => {
     const companyImg = document.getElementById('upload-company');
     const userImg = document.getElementById('upload-user');
 
-    /**Sets Croppie library with the images that the user had, 
-    *  being the first parameter the route to the company image
-    *  And the second parameter the users image
-    */    
-    setDefaultImages('../resources/alala.jpg', '../resources/alala.jpg');
-
     //Changes the image to crop to the one the user uploads
-    companyImg.addEventListener('change', (event) => {
-        changeImage(URL.createObjectURL(event.target.files[0]), 'croppieCompany');
-    });
+     companyImg.addEventListener('change', (event) => {
+         changeImage(URL.createObjectURL(event.target.files[0]), 'croppieCompany');
+     });
 
     userImg.addEventListener('change', (event) => {
         changeImage(URL.createObjectURL(event.target.files[0]), 'croppieUser');
