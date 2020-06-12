a46506ac33acb911319d57f7d9514903f1ecf363
josemoncada87 <josemoncada87@hotmail.com>
2020-06-12 12:05:31 -0500

    update for address

--- scripts/samples/sample_editProfileInformation.js
+++ scripts/samples/sample_editProfileInformation.js
@@ -2,12 +2,13 @@ window.addEventListener('load', () => {
 
     const info = {
         companyName: 'Colgate',
-        nit: '800.137.960-7',
+        nit: '800137960',
         username: 'Andrés Trujillo',
         number: '3008200838',
         email: 'andres.trujillo@mimarca.com.co',
         sector: 'O1',
         city: 'O3',
+        address: 'Calle 13 # 53-45 Piso 4',
     }
 
     editProfileInformation(info);
