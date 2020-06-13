1b1c0020833e95366fea5c3e376f6b02003dd244
juanbravozu <juanda.bravo98@gmail.com>
2020-06-12 20:07:30 -0500

    Change img Button

--- scripts/createProfileImageModal.js
+++ scripts/createProfileImageModal.js
@@ -1,5 +1,7 @@
 let changeImage = (imgUrl, id) => {};
 let setDefaultImages = (imgCompany, imgUser) => {};
+let getCompanyCroppie = () => {};
+let getUserCroppie = () => {};
 
 window.addEventListener('load', () => {
 
@@ -10,50 +12,46 @@ window.addEventListener('load', () => {
     //This function changes the image of it's respective component to the one specified in its parameters
     changeImage = (imgUrl, id) => {
         if(id == 'croppieCompany') {
-            if(company) company.destroy();
+            if(company) {
+                company.destroy();
+            } else {
+                const container = document.querySelector('.register__editPhoto--firstPart');
+                const updateBtn = document.createElement('label');
+                updateBtn.classList.add('btn', 'btn--small', 'btn--update');
+                updateBtn.setAttribute('for', 'update-company');
+                updateBtn.innerHTML = 'Cambiar imagen';
+                container.appendChild(updateBtn);
+            }
             company = new Croppie(document.getElementById(id), { viewport: { width: 150, height: 150, type: 'circle' }});
             company.bind({
                 url: imgUrl,
             });
         } else {
-            if(user) user.destroy();
+            if(user) {
+                user.destroy();
+            } else {
+                const container = document.querySelector('.register__editPhoto--secondPart');
+                const updateBtn = document.createElement('label');
+                updateBtn.classList.add('btn', 'btn--small', 'btn--update');
+                updateBtn.setAttribute('for', 'update-user');
+                updateBtn.innerHTML = 'Cambiar imagen';
+                container.appendChild(updateBtn);
+                container.style.marginBottom = '90px';
+            }
             user = new Croppie(document.getElementById(id), { viewport: { width: 150, height: 150, type: 'circle' }});
             user.bind({
                 url: imgUrl,
             });
         }
-    };
+    };    
 
-    setDefaultImages = (imgCompany, imgUser) => {
+    getCompanyCroppie = () => {
+        if(company) return company;
+        return null
+    }
 
-        //--------------------------------- Modal for company profile image
-        const companyImgChange = document.getElementById('companyImgChange');
-        const companyImgModal = document.getElementById('companyImgModal');
-        const companyModalClose = companyImgModal.querySelector('.popUp__contentClose');
-    
-        companyImgChange.addEventListener('click', () => {
-            companyImgModal.classList.remove('popUp--inactive');
-            if(!company) changeImage(imgCompany, 'croppieCompany');
-        });
-    
-        companyModalClose.addEventListener('click', () => {
-            companyImgModal.classList.add('popUp--inactive');
-        });
-
-        //---------------------------------- Modal for user profile image
-        const userImgChange = document.getElementById('userImgChange');
-        const userImgModal = document.getElementById('userImgModal');
-        const userModalClose = userImgModal.querySelector('.popUp__contentClose');
-    
-        if( userImgChange!== null )
-        userImgChange.addEventListener('click', () => {
-            userImgModal.classList.remove('popUp--inactive');
-            if(!user) changeImage(imgUser, 'croppieUser');
-        });
-    
-        userModalClose.addEventListener('click', () => {
-            userImgModal.classList.add('popUp--inactive');
-        });
-    };
-    
+    getUserCroppie = () => {
+        if(user) return user;
+        return null
+    }
 });
--- scripts/samples/sample_createProfileImageModal.js
+++ scripts/samples/sample_createProfileImageModal.js
@@ -2,12 +2,17 @@ window.addEventListener('load', () => {
 
     //Both inputs type file for their respective profile images
     const companyImg = document.getElementById('upload-company');
+    const updateCompanyImg = document.getElementById('update-company');
     const userImg = document.getElementById('upload-user');
 
     //Changes the image to crop to the one the user uploads
      companyImg.addEventListener('change', (event) => {
          changeImage(URL.createObjectURL(event.target.files[0]), 'croppieCompany');
      });
+
+     updateCompanyImg.addEventListener('change', (event) => {
+        changeImage(URL.createObjectURL(event.target.files[0]), 'croppieCompany');
+    });
 
     userImg.addEventListener('change', (event) => {
         changeImage(URL.createObjectURL(event.target.files[0]), 'croppieUser');
